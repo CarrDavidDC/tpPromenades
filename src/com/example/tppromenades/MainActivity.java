d5d8148bad1726a69c07e1dc44ebe1a3dfcf0e53
David CARRASCO <david.carrasco.dc@gmail.com>
2013-12-25 02:32:54 +0100

    for google maps (no localisation)

--- src/com/example/tppromenades/MainActivity.java
+++ src/com/example/tppromenades/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.tppromenades;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+public class MainActivity extends FragmentActivity {
 
-public class MainActivity extends Activity {
-
+	 static final LatLng MUMBAI = new LatLng(19.0144100, 72.8479400);
+     private GoogleMap map;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		System.out.println("test");
+        setContentView(R.layout.activity_main);
+       // map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+     //   map.moveCamera(CameraUpdateFactory.newLatLngZoom(MUMBAI, 15));
+ 
+        // Zoom in, animating the camera.
+     //   map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 	}
 
 	@Override
